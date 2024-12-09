db5852515629ba00bf0f61ce82a90d10a6849dce
LawrenciaKlu <you@example.com>
2024-12-09 12:04:23 -0800

    adding all changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-let last = document.getElementById("last");
-last.innerHTML = "I am going to miss you all 😎";
+// let last = document.getElementById("last");
+// last.innerHTML = "I am going to miss you all 😎";
 
 // let image = document.querySelector("img");
-// image.innerHTML = "<img c1 src = 'images/bb.png'>";
-function changebuttonStyle(){
-    button.style.color = "white";
-    button.style.backgroundColor = "purple";
+// // image.innerHTML = "<img c1 src = 'images/bb.png'>";
+// function changebuttonStyle(){
+//     button.style.color = "white";
+//     button.style.backgroundColor = "purple";
+// }
+
+// let button = document.querySelector("button");// selecting the element..the event that should listen frohappen
+// button.addEventListener("mouseover", changebuttonStyle);
+// button.addEventListener("mouseout", changebuttonStyle);
+
+
+function showItem(){
+    let image = document.querySelector(".imageone");
+    image.style.display = "block";
+    let close = document.querySelector(".close");
+    close.style.display = "block";
+}
+let open = document.querySelector(".open");
+open.addEventListener("click", showItem);
+
+function hideItem(){
+    let image = document.querySelector(".imageone");
+    image.style.display = "none";
+    let close = document.querySelector(".close");
+    close.style.display = "none";
 }
 
-let button = document.querySelector("button");// selecting the element..the event that should listen frohappen
-button.addEventListener("mouseover", changebuttonStyle);
-button.addEventListener("mouseout", changebuttonStyle);
-
+let close = document.querySelector(".close");
+close.addEventListener("click", hideItem);
+    
 
